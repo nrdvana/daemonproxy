6f822952b8cfc801a12e17a7f9c38fefb073dc0f
Michael Conrad <mike@nrdvana.net>
2014-03-01 14:53:58 -0500

    Updated Test::DaemonProxy to support arbitrary pipes
    This is in anticipation of new feature where any number of
    file descriptors can be passed to daemonproxy and it makes
    them available as fd_# objects.

--- t/lib/Test/DaemonProxy.pm
+++ t/lib/Test/DaemonProxy.pm
@@ -34,46 +34,49 @@ sub temp_path {
 
 sub new {
 	my $class= shift;
-	return bless { timeout => 0.5, dp_stdout_buf => '', dp_stderr_buf => '' }, $class;
+	return bless { timeout => 0.5 }, $class;
 }
 
-sub dp_pid      { $_[0]{dp_pid} }
+sub dp_pid        { $_[0]{dp_pid} }
 *pid= *dp_pid;
-sub dp_stdin    { $_[0]{dp_stdin} }
-sub dp_stdout   { $_[0]{dp_stdout} }
-sub dp_stderr   { $_[0]{dp_stderr} }
+sub dp_fds        { $_[0]{dp_fds} }
+sub dp_stdin      { $_[0]{dp_fds}[0]{handle} }
+sub dp_stdout     { $_[0]{dp_fds}[1]{handle} }
+sub dp_stderr     { $_[0]{dp_fds}[2]{handle} }
 sub last_captures { $_[0]{last_captures} }
-sub timeout     { @_ > 1? ($_[0]{timeout}= $_[1]) : $_[0]{timeout}; }
+sub timeout       { @_ > 1? ($_[0]{timeout}= $_[1]) : $_[0]{timeout}; }
 
 sub run {
 	my ($self, @argv)= @_;
 	die "Run was already called on this object"
 		if defined $self->{dp_pid};
-	
-	pipe(my ($dp_stdin_r,  $dp_stdin_w))  or die "pipe: $!";
-	pipe(my ($dp_stdout_r, $dp_stdout_w)) or die "pipe: $!";
-	pipe(my ($dp_stderr_r, $dp_stderr_w)) or die "pipe: $!";
+	my @parent_fds;
+	my @child_fds;
+	my $pipe_count= 3;
+	for (my $i= 0; $i < $pipe_count; $i++) {
+		pipe(my ($r,  $w)) or die "pipe: $!";
+		push @parent_fds, { handle => $i? $r : $w, input => $i == 0, output => $i != 0, buffer => '' };
+		push @child_fds,  $i? $w : $r;
+	}
 	
 	# Launch DaemonProxy instance
 	defined ($self->{dp_pid}= fork()) or die "fork: $!";
 	if ($self->dp_pid == 0) {
-		open STDIN,  ">&", $dp_stdin_r  or die "dup pipe to stdin: $!";
-		open STDOUT, ">&", $dp_stdout_w or die "dup pipe to stdout: $!";
-		open STDERR, ">&", $dp_stderr_w or die "dup pipe to stderr: $!";
+		for (my $i= 0; $i < $pipe_count; $i++) {
+			POSIX::dup2(fileno($child_fds[$i]), $i)
+				or die "dup pipe to $i: $!";
+		}
 		exec($self->binary_path, @argv)
 			or warn "exec(daemonproxy): $!";
 		# make a sharp exit, without running cleanup code that could interfere with the parent process
-		exec('/bin/false') || 	POSIX::_exit(2);
+		exec('/bin/false') || POSIX::_exit(2);
 	}
 	
-	$self->{dp_stdin}= $dp_stdin_w;
-	$self->{dp_stdin}->autoflush(1);
-	
-	$self->{dp_stdout}= $dp_stdout_r;
-	$self->{dp_stdout}->blocking(0);
-	
-	$self->{dp_stderr}= $dp_stderr_r;
-	$self->{dp_stderr}->blocking(0);
+	for (@parent_fds) {
+		$_->{handle}->autoflush(1) if $_->{input};
+		$_->{handle}->blocking(0)  if $_->{output};
+	}
+	$self->{dp_fds}= \@parent_fds;
 	1;
 }
 
@@ -108,21 +111,20 @@ sub send {
 sub _read_more {
 	my ($self, $inputs)= @_;
 	my %ready= map { $_ => 1 }
-		IO::Select->new( grep { defined } map { ${$_->[0]} } @$inputs )
+		IO::Select->new( grep { defined } map { $_->{handle} } @$inputs )
 			->can_read($self->timeout);
 	my $result= 0;
 	for (@$inputs) {
-		my ($fd_ref, $buf_ref)= @$_;
-		next unless defined $$fd_ref and $ready{$$fd_ref};
+		next unless defined $_->{handle} and $ready{$_->{handle}};
 		$result= 1;
-		my $got= sysread($$fd_ref, $$buf_ref, 1024, length($$buf_ref));
+		my $got= sysread($_->{handle}, $_->{buffer}, 1024, length($_->{buffer}));
 		if (!$got) {
-			close($$fd_ref);
-			$$fd_ref= undef;
+			close($_->{handle});
+			$_->{handle}= undef;
 			next;
 		}
 		Test::More::note("recv: "._quote_str($_))
-			for (substr($$buf_ref, -$got) =~ /^.*$/mg);
+			for (substr($_->{buffer}, -$got) =~ /^.*$/mg);
 	}
 	return $result;
 }
@@ -149,12 +151,11 @@ sub _recv_pattern {
 			unless "$pattern" =~ /^\(\?\^?[a-ln-oq-z]*p[a-ln-oq-z]*m/;
 	}
 	while (1) {
-		for (@$inputs) {
-			my ($io_ref, $buf_ref)= @$_;
+		for my $input (@$inputs) {
 			for (my $p= 0; $p < @$patterns; $p++) {
 				# Check each pattern against the buffer
 				my @captures;
-				if (@captures= ($$buf_ref =~ $patterns->[$p])) {
+				if (@captures= ($input->{buffer} =~ $patterns->[$p])) {
 					my $match= ${^MATCH};
 					my $line_end= -1;
 					# Make sure we have the whole line it matched on
@@ -162,14 +163,14 @@ sub _recv_pattern {
 						$line_end= length(${^PREMATCH}) + length($match);
 					} elsif (index(${^POSTMATCH}, "\n") >= 0) {
 						$line_end= length(${^PREMATCH}) + length($match) + index(${^POSTMATCH}, "\n") + 1;
-					} elsif (!defined $$io_ref) {
-						$line_end= length($$buf_ref);
+					} elsif (!defined $input->{handle}) {
+						$line_end= length($input->{buffer});
 					} else {
 						next;
 					}
 					# Now remove all buffer up to and including the line the match occurred on.
-					my $removed= $line_end > 0? substr($$buf_ref, 0, $line_end) : '';
-					substr($$buf_ref, 0, $line_end)= '';
+					my $removed= $line_end > 0? substr($input->{buffer}, 0, $line_end) : '';
+					substr($input->{buffer}, 0, $line_end)= '';
 					$self->{last_match}= $match;
 					$self->{last_pattern_idx}= $p;
 					$self->{last_input_removed}= $removed;
@@ -178,60 +179,43 @@ sub _recv_pattern {
 				}
 			}
 		}
-		$self->_read_more($inputs)
+		$self->_read_more([ grep { $_->{output} } @{$self->{dp_fds}} ])
 			or return 0;
 	}
 }
 
 sub discard_response {
 	my $self= shift;
-	while ($self->_read_more([
-		[ \$self->{dp_stdout}, \$self->{dp_stdout_buf} ],
-		[ \$self->{dp_stderr}, \$self->{dp_stderr_buf} ]
-		])) {}
-	$self->{dp_stdout_buf}= '';
-	$self->{dp_stderr_buf}= '';
+	while ($self->_read_more([ @{$self->{dp_fds}}[1,2] ])) {}
+	$self->{dp_fds}[1]{buffer}= '';
+	$self->{dp_fds}[2]{buffer}= '';
 }
 
 sub discard_stdout {
 	my $self= shift;
-	while ($self->_read_more([
-		[ \$self->{dp_stdout}, \$self->{dp_stdout_buf} ],
-		])) {}
-	$self->{dp_stdout_buf}= '';
+	while ($self->_read_more([ @{$self->{dp_fds}}[1] ])) {}
+	$self->{dp_fds}[1]{buffer}= '';
 }
 
 sub discard_stderr {
 	my $self= shift;
-	while ($self->_read_more([
-		[ \$self->{dp_stderr}, \$self->{dp_stderr_buf} ]
-		])) {}
-	$self->{dp_stderr_buf}= '';
+	while ($self->_read_more([ @{$self->{dp_fds}}[2] ])) {}
+	$self->{dp_fds}[2]{buffer}= '';
 }
 
 sub recv {
 	my ($self, @patterns)= @_;
-	my $inputs= [
-		[ \$self->{dp_stdout}, \$self->{dp_stdout_buf} ],
-		[ \$self->{dp_stderr}, \$self->{dp_stderr_buf} ]
-		];
-	$self->_recv_pattern($inputs, \@patterns);
+	$self->_recv_pattern([ @{$self->{dp_fds}}[1,2] ], \@patterns);
 }
 
 sub recv_stdout {
 	my ($self, @patterns)= @_;
-	my $inputs= [
-		[ \$self->{dp_stdout}, \$self->{dp_stdout_buf} ],
-		];
-	$self->_recv_pattern($inputs, \@patterns);
+	$self->_recv_pattern([ @{$self->{dp_fds}}[1] ], \@patterns);
 }
 
 sub recv_stderr {
 	my ($self, @patterns)= @_;
-	my $inputs= [
-		[ \$self->{dp_stderr}, \$self->{dp_stderr_buf} ]
-		];
-	$self->_recv_pattern($inputs, \@patterns);
+	$self->_recv_pattern([ @{$self->{dp_fds}}[2] ], \@patterns);
 }
 
 sub recv_ok {
@@ -241,8 +225,8 @@ sub recv_ok {
 	if ($self->recv($pattern)) {
 		goto &Test::More::pass;
 	} else {
-		Test::More::diag("stdout buffer: ".Data::Dumper->new([ $self->{dp_stdout_buf} ])->Terse(1)->Dump);
-		Test::More::diag("stderr buffer: ".Data::Dumper->new([ $self->{dp_stderr_buf} ])->Terse(1)->Dump);
+		Test::More::diag("stdout buffer: ".Data::Dumper->new([ $self->{dp_fds}[1]{buffer} ])->Terse(1)->Dump);
+		Test::More::diag("stderr buffer: ".Data::Dumper->new([ $self->{dp_fds}[2]{buffer} ])->Terse(1)->Dump);
 		goto &Test::More::fail;
 	}
 }
@@ -255,7 +239,7 @@ sub recv_stdout_ok {
 	if ($self->recv_stdout($pattern)) {
 		goto &Test::More::pass;
 	} else {
-		Test::More::diag("stdout buffer: ".Data::Dumper->new([ $self->{dp_stdout_buf} ])->Terse(1)->Dump);
+		Test::More::diag("stdout buffer: ".Data::Dumper->new([ $self->{dp_fds}[1]{buffer} ])->Terse(1)->Dump);
 		goto &Test::More::fail;
 	}
 }
@@ -267,7 +251,7 @@ sub recv_stderr_ok {
 	if ($self->recv_stderr($pattern)) {
 		goto &Test::More::pass;
 	} else {
-		Test::More::diag("stderr buffer: ".Data::Dumper->new([ $self->{dp_stderr_buf} ])->Terse(1)->Dump);
+		Test::More::diag("stderr buffer: ".Data::Dumper->new([ $self->{dp_fds}[2]{buffer} ])->Terse(1)->Dump);
 		goto &Test::More::fail;
 	}
 }
